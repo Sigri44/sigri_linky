34827087bbc560ef8235cb6a12736d24290ba73d
Sigri44 <sigri44@hotmail.fr>
2018-06-12 07:53:50 +0200

    correction faute orthographe

--- sigri_linky/plugin_info/configuration.php
+++ sigri_linky/plugin_info/configuration.php
@@ -28,9 +28,9 @@
         <label class="col-lg-4 control-label">{{Fonctionnement du plugin}}</label>
         <div class="col-lg-5">
           Ce plugin utilise le site Enedis pour obtenir les informations de votre consommation depuis votre compteur Linky<br>
-          Si vous n'avaez pas encore de compte Enedis, vous pouvez l'ouvrir 
-          <a href="https://espace-client-particuliers.enedis.fr/web/espace-particuliers/creation-de-compte" target="_blank" > en cliquant ici</a><br>.
-          Ce plugin historise vos donnees, vous devez avoir donc un equiepement non visible pour ne pas l'avoir en dashboard.<br>
+          Si vous n'avez pas encore de compte Enedis, vous pouvez l'ouvrir 
+          <a href="https://espace-client-particuliers.enedis.fr/web/espace-particuliers/creation-de-compte" target="_blank" > en cliquant ici</a>.<br>
+          Ce plugin historise vos donnees, vous devez avoir donc un equipement non visible pour ne pas l'avoir en dashboard.<br>
           Pour acceder aux donnees : creer une vue, un design pour charger le graphique, ou consulter l'historique.
         </div>
       </div>
